c962f18cdaa710853884e843c49fca5f561f7a4c
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2018-06-13 19:12:21 -0300

    Main Frame created

--- src/jogopoo/JogoPoo.java
+++ src/jogopoo/JogoPoo.java
@@ -1,26 +1,50 @@
 package jogopoo;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class JogoPoo {
 
-    public final static int ONE_SECOND = 1000;
+    public final static int LOOP_DURATION = 30;
     
     public static void main(String[] args) {
-       loop();
+        //<Awake>
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createFrame();
+            }
+        });
+        //</Awake>
+
+        loop();
     }
   
     public static void update(){
-        System.out.println("oi");
+        
+    }
+    
+    private static void createFrame() {
+        JFrame frame = new JFrame("Crackpots");
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(800,600));
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+        
+        /*ImageIcon icon = new ImageIcon("/images/sprite.png");
+        JLabel label = new JLabel(icon);
+        frame.getContentPane().add(label);*/
     }
     
     public static void loop(){
         long expectedtime = System.currentTimeMillis();
+        
         while (true) {
-           if(System.currentTimeMillis() >= expectedtime){
-            expectedtime += ONE_SECOND;
-            update();
-           }
+            if(System.currentTimeMillis() >= expectedtime){
+                expectedtime += LOOP_DURATION;
+                update();
+            }
         }
     }
 }
